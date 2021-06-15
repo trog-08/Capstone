7abe48bc1b50869bfffbd22525d15df7c5666caf
Ben Arthur <81194806+trog-08@users.noreply.github.com>
2021-06-15 23:40:36 +1000

    Add files via upload

---
+++ src/client/index.js
@@ -0,0 +1,19 @@
+import {
+    getGeonames,
+    getWeatherBit,
+    getPixabay,
+    performAction,
+    uiUpdate,
+    postData,
+  } from "./js/app.js";
+  
+  import "./styles/styles.scss";
+  
+  export {
+    performAction,
+    uiUpdate,
+    postData,
+    getGeonames,
+    getWeatherBit,
+    getPixabay,
+  };
